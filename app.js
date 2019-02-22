12dcf49dccf3b1d81133208412718963880c9ff9
evan-kapantais <evankapantais@gmail.com>
2019-02-22 08:51:47 +0100

    initialize

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ var tabContent = document.getElementsByClassName('tabcontent');
 for (var i = 0; i < tabContent.length; ++i) {
     tabContent[i].style.display = 'none';
 }
+
+document.getElementById('button').addEventListener('click', switchTab);
+
+
+function resetTabs() {
+    document.getElementsByClassName('nav-link').classList.remove('active');
+}
+
+function switchTab() {
+    var input = 0;
+    
+    input = document.getElementById('input').value;
+
+    if (input < 0 || input > 3) {
+        alert('Please enter a number from 1 to 3.');
+    }
+
+    if (input == 1) {
+        
+    }
+}
